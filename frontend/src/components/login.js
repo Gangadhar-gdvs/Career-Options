4cfbbc67a2f47b89a1b4f888fd81c68039013cb3
Gooti Gangadhara <68936937+Gangadhar-gdvs@users.noreply.github.com>
2024-12-16 13:50:16 +0530

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -29,7 +29,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://career-options.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
